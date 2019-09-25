d8d3c5036c55557298c0ddc9683e7aa18806d9de
Jonathan Larouche <jlarouche@dev-id.com>
2019-09-25 14:53:32 -0400

    Add project files.

---
+++ 58027442/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _58027442
+{
+    public class ChatHub : Hub
+    {
+        static int Temperature = new Random().Next();
+
+        [HubMethodName("change_weather")]
+        public void ChangeWeather(int temperature)
+        {
+            Temperature = temperature;
+            Clients.Others.NotifyUser(temperature);
+
+        }
+
+        [HubMethodName("get_weather")]
+        public void GetWeather()
+        {
+            Clients.Caller.NotifyUser(Temperature);
+        }
+    }
+}
---
+++ 58027442/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_58027442.Startup))]
+
+namespace _58027442
+{
+    public class Startup
+    {
+        
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
